eb80422792ee56a75b77f0eda37e34d897c55375
24572 <24572@students.mrgs.school.nz>
2026-04-22 11:03:41 +1200

    adding...

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from tkinter import*
-root=Tk()
-root.title("Focus Tracker")
-root.geometry("1200x700")
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
+class Application :
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Focus Tracker")
+        self.root.geometry("1100x700")
+        self.root.configure(bg="#0b3d91")
+        self.root.resizable(False, False)
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+
+        self.H_page = tk.Frame(root, width=800, height=500, bg="#ffffff")
+        self.H_page.grid(row=0, column=0, sticky="nsew")
+
+        # Background image
+        self.home_bg_image = Image.open("images/Bg1.png")  # Make sure this image is 900x600
+        self.home_bg_image = self.home_bg_image.resize((1100, 700))
+        self.bg_photo = ImageTk.PhotoImage(self.home_bg_image)
+        self.bg_label = tk.Label(self.H_page, image=self.bg_photo)
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+root = tk.Tk()
+app = Application(root)
 root.mainloop()
