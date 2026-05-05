e33c39efa66c6ba03af2f564ad7cfb2bf977bd05
24572 <24572@students.mrgs.school.nz>
2026-05-06 10:53:19 +1200

    adding...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from PIL import Image, ImageTk
+
 
 
 class Application:
@@ -12,52 +12,64 @@ class Application:
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
 
-        #Store frames in a dictionary
+        #Store frames in a dictionary#
         self.frames = {}
 
-        #Create all pages
+        #Create all pages#
         for I in (self.open_HME, self.open_POM, self.open_STO, self.open_SET):
             frame = I()
             self.frames[I.__name__] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        #Show home first
+        #Show home first#
         self.show_frame("open_HME")
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
 
-    # HOME PAGE
+    # HOME PAGE#
     def open_HME(self):
-        frame = tk.Frame(self.root, bg="yellow")
-        title = tk.Label(frame, text="Focus Tracker", font=("Impact", 60), bg="yellow")
+        frame = tk.Frame(self.root, bg="black")
+        title = tk.Label(frame, text="FOCUS TRACKER", font=("Impact", 70), bg="black",fg="white")
         title.place(relx=0.5, y=20, anchor="n")
 
-        #BUTTONS
-        button_frame = tk.Frame(frame, bg="#ffffff")
-        button_frame.place(x=50, y=150)
+        #BUTTONS#
+        button_frame = tk.Frame(frame, bg="black")
+        button_frame.pack(expand=True)
 
-        tk.Button(button_frame, text="Pomodoro", font=("Impact", 30),command=lambda: self.show_frame("open_POM")).pack(pady=10)
-        tk.Button(button_frame, text="Stopwatch", font=("Impact", 30),command=lambda: self.show_frame("open_STO")).pack(pady=10)
-        tk.Button(button_frame, text="Settings", font=("Impact", 30),command=lambda: self.show_frame("open_SET")).pack(pady=10)
+        tk.Button(button_frame, text="  POMODORO ", font=("Impact", 40),bg="black",fg="white",command=lambda: self.show_frame("open_POM")).pack(fill="x",pady=10)
+        tk.Button(button_frame, text="  STOPWATCH", font=("Impact", 40),bg="black",fg="white",command=lambda: self.show_frame("open_STO")).pack(fill="x",pady=10)
+        tk.Button(button_frame, text="  SETTINGS ", font=("Impact", 40),bg="black",fg="white",command=lambda: self.show_frame("open_SET")).pack(fill="x",pady=10)
         return frame
 
-    #POMODORO
+    #POMODORO#
     def open_POM(self):
         frame = tk.Frame(self.root, bg="blue")
-        tk.Label(frame, text="Pomodoro", font=("Impact", 40), bg="blue").pack(pady=20)
-        tk.Button(frame, text="Back", font=("Impact", 20),command=lambda: self.show_frame("open_HME")).pack()
+        frame.grid_columnconfigure(0, weight=1)
+
+        tk.Label(frame, text="POMODORO", font=("Impact", 70), bg="blue") \
+            .grid(row=0, column=0, pady=20)
+
+        #BUTTONS#
+        button_frame = tk.Frame(frame, bg="blue")
+        button_frame.grid(row=1, column=0)
+
+        tk.Button(button_frame, text="LONG BREAK", font=("Impact", 40),command=lambda: self.show_frame("open_HME")).grid(row=0, column=0, padx=10)
+        tk.Button(button_frame, text="SHORT BREAK", font=("Impact",40),command=lambda: self.show_frame("open_HME")).grid(row=0, column=1, padx=10)
+        tk.Button(button_frame, text="BACK", font=("Impact", 40),command=lambda: self.show_frame("open_HME")).grid(row=0, column=2, padx=10)
+
         return frame
 
-    #STOPWATCH
+    #STOPWATCH#
     def open_STO(self):
         frame = tk.Frame(self.root, bg="green")
         tk.Label(frame, text="Stopwatch", font=("Impact", 40), bg="green").pack(pady=20)
+
         tk.Button(frame, text="Back", font=("Impact", 20),command=lambda: self.show_frame("open_HME")).pack()
         return frame
 
-    #SETTINGS
+    #SETTINGS#
     def open_SET(self):
         frame = tk.Frame(self.root, bg="red")
         tk.Label(frame, text="Settings", font=("Impact", 40), bg="red").pack(pady=20)
